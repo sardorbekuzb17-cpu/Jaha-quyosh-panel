4d5c03b849f93dae0b4575be9abfae794571e103
Maxmudov Sardor <sardor@example.com>
2026-04-15 01:57:10 +0500

    Payme: PerformTransaction - tranzaksiya topilmasa -31003 qaytarish

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -141,10 +141,8 @@ function performTransaction(params) {
     const { id } = params;
 
     // Sandbox test uchun: maxsus ID lar
-    // Agar ID "69dea9665e5e8dad8f3b72c2" bo'lsa - bekor qilingan tranzaksiya
-    if (id === '69dea9665e5e8dad8f3b72c2') {
-        return { error: ERRORS.CANT_PERFORM };
-    }
+    // Agar ID "69dea9665e5e8dad8f3b72c2" bo'lsa va tranzaksiya mavjud emas - topilmadi
+    // Agar ID "69dea9665e5e8dad8f3b72c2" bo'lsa va tranzaksiya mavjud - bekor qilingan
 
     const tx = transactions.get(id);
 
